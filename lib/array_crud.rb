2e941d61c6cf1d15c8831735e981a88e0922ba3e
Abel Quintero <abel.j.quintero@gmail.com>
2018-03-13 00:14:45 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -12,8 +12,7 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  array = [" ", " "]
-  array.unshift("wow")
+  array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
@@ -21,7 +20,7 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-
+  array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
